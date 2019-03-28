d667a91de5a3238031a1dce8349412259d06fef4
suihailiang <38457486+suihailiang@users.noreply.github.com>
2019-03-28 15:35:05 +0800

    Update test.py

--- test.py
+++ test.py
@@ -5,14 +5,13 @@ client = pymongo.MongoClient(host='localhost', port=27017)
 db = client.db1
 #在数据db1中创建collection名为mid_fingertable_notice
 collection=db.support_retrieve2notice
-# count=collection.count()
-# print(count)
+
 data=collection.find({"fragment_type" : "01"}).limit(2000)
 for json_data in data:
     for json_data1 in json_data["content"]:
         page=json_data1["page"]
         
-        print("xingzeng")
+ 
         print(page)
         
         
